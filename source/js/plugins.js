e93269837131e792b9eb6b29d42772f71367b5cc
zhnlk <tomleader0828@gmail.com>
2016-07-10 17:32:21 +0800

    reformat js files except min.js

--- source/js/plugins.js
+++ source/js/plugins.js
@@ -1,79 +1,79 @@
-(function(){
-  'use strict';
+(function () {
+    'use strict';
 
-  var elements = document.getElementsByClassName('plugin');
-  var $count = document.getElementById('plugin-list-count');
-  var $input = document.getElementById('plugin-search-input');
-  var elementLen = elements.length;
-  var index = lunr.Index.load(window.SEARCH_INDEX);
+    var elements = document.getElementsByClassName('plugin');
+    var $count = document.getElementById('plugin-list-count');
+    var $input = document.getElementById('plugin-search-input');
+    var elementLen = elements.length;
+    var index = lunr.Index.load(window.SEARCH_INDEX);
 
-  function updateCount(count){
-    $count.innerHTML = count + (count === 1 ? ' item' : ' items');
-  }
-
-  function addClass(elem, className){
-    var classList = elem.classList;
-
-    if (!classList.contains(className)){
-      classList.add(className);
-    }
-  }
-
-  function removeClass(elem, className){
-    var classList = elem.classList;
-
-    if (classList.contains(className)){
-      classList.remove(className);
-    }
-  }
-
-  function search(value){
-    var result = index.search(value);
-    var len = result.length;
-    var selected = {};
-    var i = 0;
-
-    for (i = 0; i < len; i++){
-      selected[result[i].ref] = true;
+    function updateCount(count) {
+        $count.innerHTML = count + (count === 1 ? ' item' : ' items');
     }
 
-    for (i = 0; i < elementLen; i++){
-      if (selected[i]){
-        addClass(elements[i], 'on');
-      } else {
-        removeClass(elements[i], 'on');
-      }
+    function addClass(elem, className) {
+        var classList = elem.classList;
+
+        if (!classList.contains(className)) {
+            classList.add(className);
+        }
     }
 
-    updateCount(len);
-  }
+    function removeClass(elem, className) {
+        var classList = elem.classList;
 
-  function displayAll(){
-    for (var i = 0; i < elementLen; i++){
-      addClass(elements[i], 'on');
+        if (classList.contains(className)) {
+            classList.remove(className);
+        }
     }
 
-    updateCount(elements.length);
-  }
+    function search(value) {
+        var result = index.search(value);
+        var len = result.length;
+        var selected = {};
+        var i = 0;
 
-  function hashchange(){
-    var hash = location.hash.substring(1);
-    $input.value = hash;
+        for (i = 0; i < len; i++) {
+            selected[result[i].ref] = true;
+        }
 
-    if (hash){
-      search(hash);
-    } else {
-      displayAll();
+        for (i = 0; i < elementLen; i++) {
+            if (selected[i]) {
+                addClass(elements[i], 'on');
+            } else {
+                removeClass(elements[i], 'on');
+            }
+        }
+
+        updateCount(len);
     }
-  }
 
-  $input.addEventListener('input', function(){
-    var value = this.value;
+    function displayAll() {
+        for (var i = 0; i < elementLen; i++) {
+            addClass(elements[i], 'on');
+        }
 
-    if (!value) return displayAll();
-    search(value);
-  });
+        updateCount(elements.length);
+    }
 
-  window.addEventListener('hashchange', hashchange);
-  hashchange();
+    function hashchange() {
+        var hash = location.hash.substring(1);
+        $input.value = hash;
+
+        if (hash) {
+            search(hash);
+        } else {
+            displayAll();
+        }
+    }
+
+    $input.addEventListener('input', function () {
+        var value = this.value;
+
+        if (!value) return displayAll();
+        search(value);
+    });
+
+    window.addEventListener('hashchange', hashchange);
+    hashchange();
 })();
